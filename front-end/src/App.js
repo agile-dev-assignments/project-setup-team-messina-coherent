e2e094e0b746803c0bb837ad1a4da0152558dbcd
Aishwarya Manojkumar <amm1559@nyu.edu>
2021-03-20 18:15:15 -0400

    Implementing privacy policy page from user story 7

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,25 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
-import Contact from './contact';
+import Privacy from './Privacy.js'
+import Headers from './Privacy_Header.js'
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
 
 function App() {
   return (
-    <div className="App">
-      <Contact />
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Headers />
+      <Privacy />
+    
     </div>
   );
 }
