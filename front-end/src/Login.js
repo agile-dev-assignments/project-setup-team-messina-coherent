03abb53d86e9cf8a9698e6252e8a73c7cf60a89d
RubyMamba <78879000+RubyMamba@users.noreply.github.com>
2021-03-22 15:26:37 -0400

    Add files via upload

---
+++ front-end/src/Login.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './Login.css';
+function (Login)  {
+
+<button onSumbit>Spotify Login</button>{
+
+    Login-button {
+        background-color: #57eb07;
+        min-height: 100vh;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        font-size: calc(10px + 2vmin);
+        color: white;
+      }
+
+    
+    }
+}
