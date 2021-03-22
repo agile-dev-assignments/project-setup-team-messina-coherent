38290ea8591e1d18e044a40ddd16316db929ac90
ryan-schwarz <45295616+ryan-schwarz@users.noreply.github.com>
2021-03-22 15:50:03 -0400

    Made home page

--- front-end/src/faq.js
+++ front-end/src/faq.js
@@ -1,20 +1,20 @@
 import './faq.css';
 
-function faq(){
-    return(
-    <div class="searchbar">
-        <h1>FAQ Page</h1>
-        <form method="" action="">
-            <input class="content" type="text" name="search" placeholder="Search all contents"></input>
-            <input class="button" type="submit" value="search"></input>
-        </form>
-        <table class='questiontable'>
-            <tr class='first_row'>
-                <th style='font-size:12px;'>Questions</th>
-                <th style='font-size:12px;'>Answer</th>
-            </tr>
-        </table>
-       </div>
-    );
+function FAQ() {
+  return (
+    <div class='searchbar'>
+      <h1>FAQ Page</h1>
+      <form method='' action=''>
+        <input className='content' type='text' name='search' placeholder='Search all contents'></input>
+        <input className='button' type='submit' value='search'></input>
+      </form>
+      <table class='questiontable'>
+        <tr class='first_row'>
+          <th style={{ fontSize: '12px;' }}>Questions</th>
+          <th style={{ fontSize: '12px;' }}>Answer</th>
+        </tr>
+      </table>
+    </div>
+  );
 }
-export default faq;
+export default FAQ;
--- front-end/src/sidebar.js
+++ front-end/src/sidebar.js
@@ -6,29 +6,54 @@ import Accordions from './Accordions.js';
 
 function Sidebar() {
   const [logged, setLogin] = useState(true);
-  var arr1 = ['Home', 'My Taste', 'Playlists'];
-  var arr2 = ['Home'];
   const accordionData2 = [
     {
       question: 'By Mood',
-      answer: <><a className='menu-item'href='/byMood/playlist'>Mood Boosters</a><br></br>
-      <a className='menu-item'href='/byMood/playlist'>In My Feels</a><br></br>
-      <a className='menu-item'href='/byMood/playlist'>Romantic</a><br></br>
-      <a className='menu-item'href='/byMood/playlist'>On My Grind</a><br></br>
-      <a className='menu-item'href='/byMood/playlist'>Plotting My Revenge</a><br></br>
-      <a className='menu-item'href='/byMood/playlist'>Party</a><br></br></>
-    }
-  ]
+      answer: (
+        <>
+          <a className='menu-item' href='/byMood/playlist'>
+            Mood Boosters
+          </a>
+          <br></br>
+          <a className='menu-item' href='/byMood/playlist'>
+            In My Feels
+          </a>
+          <br></br>
+          <a className='menu-item' href='/byMood/playlist'>
+            Romantic
+          </a>
+          <br></br>
+          <a className='menu-item' href='/byMood/playlist'>
+            On My Grind
+          </a>
+          <br></br>
+          <a className='menu-item' href='/byMood/playlist'>
+            Plotting My Revenge
+          </a>
+          <br></br>
+          <a className='menu-item' href='/byMood/playlist'>
+            Party
+          </a>
+          <br></br>
+        </>
+      ),
+    },
+  ];
 
   const accordionData = [
     {
       question: 'Playlists',
-      answer: <><a className='menu-item' href='/byWeather'>By Weather</a><br style={{height:
-      "10px"}}></br>
-      <Accordions data={accordionData2} duration={400} closeOthers opened={1} /></>
-    }
-  ]
-
+      answer: (
+        <>
+          <a className='menu-item' href='/byWeather'>
+            By Weather
+          </a>
+          <br style={{ height: '10px' }}></br>
+          <Accordions data={accordionData2} duration={400} closeOthers opened={1} />
+        </>
+      ),
+    },
+  ];
 
   if ({ logged }) {
     return (
@@ -43,17 +68,18 @@ function Sidebar() {
         <a id='login' className='menu-item' href='/login'>
           Login
         </a>
-
       </Menu>
     );
   }
-  <Menu>
-    <a className='menu-item' href='/'>
-      Home
-    </a>
-    <a className='menu-item' href='/logout'>
-          Logout
-    </a>
-  </Menu>;
+  return (
+    <Menu>
+      <a className='menu-item' href='/'>
+        Home
+      </a>
+      <a className='menu-item' href='/logout'>
+        Logout
+      </a>
+    </Menu>
+  );
 }
 export default Sidebar;
