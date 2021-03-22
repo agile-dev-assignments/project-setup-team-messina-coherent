7bc6939a8b91a64ee414027e9415773126516333
ryan-schwarz <45295616+ryan-schwarz@users.noreply.github.com>
2021-03-21 23:05:44 -0400

    Most of header and side done, need to do container

--- front-end/src/navbar.js
+++ front-end/src/navbar.js
@@ -1,4 +1,3 @@
-import { slide as Menu } from "react-burger-menu";
 import './navbar.css';
 import Sidebar from './sidebar.js';
 
@@ -8,7 +7,7 @@ function NavBar(){
         <div id="navbar">
               <Sidebar />
               <ul id="nav">
-                <li><a class="logo" href="#"><img src="/logo.png"/></a></li>
+                <li><a class="logo" href="/"><img src="/logo.png"/></a></li>
               </ul>
         </div>
     );
