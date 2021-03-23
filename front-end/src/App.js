d17afda1e35d25d4186c96cbb100178c4ee3d61c
ryan-schwarz <45295616+ryan-schwarz@users.noreply.github.com>
2021-03-23 17:54:34 -0400

    Footer done

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import NavBar from './navbar.js';
+import Footer from './footer.js';
 import { BrowserRouter as Router, Switch, Route, Link, useRouteMatch, useParams } from 'react-router-dom';
 import About from './About.js';
 import Privacy from './Privacy.js';
@@ -53,7 +54,7 @@ function App() {
         </Switch>
         </div>
 
-        
+        <Footer/>
       </div>
     </Router>
   );
