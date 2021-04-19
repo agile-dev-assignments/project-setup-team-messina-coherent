d5df6a01841c55e01cc1119f0c5c2dde13b06592
ryan-schwarz <45295616+ryan-schwarz@users.noreply.github.com>
2021-04-19 19:14:43 -0400

    My taste is fully operational. Waiting on Gi to finish the login to test with users' saved tracks.

--- back-end/app.js
+++ back-end/app.js
@@ -7,6 +7,10 @@ const morgan = require('morgan'); // middleware for nice logging of incoming HTT
 // use the morgan middleware to log all incoming http requests
 //app.use(morgan('dev')); // morgan has a few logging default styles - dev is a nice concise color-coded style
 
+var cors = require('cors');
+
+app.use(cors());
+
 // use the bodyparser middleware to parse any data included in a request
 app.use(express.json()); // decode JSON-formatted incoming POST data
 app.use(express.urlencoded({ extended: true })); // decode url-encoded incoming POST data
@@ -30,17 +34,17 @@ var credentials = {
 var spotifyApi = new SpotifyWebApi(credentials);
 
 spotifyApi.clientCredentialsGrant().then(
-    function(data) {
-      console.log('The access token expires in ' + data.body['expires_in']);
-      console.log('The access token is ' + data.body['access_token']);
-  
-      // Save the access token so that it's used in future calls
-      spotifyApi.setAccessToken(data.body['access_token']);
-    },
-    function(err) {
-      console.log('Something went wrong when retrieving an access token', err);
-    }
-  );
+  function (data) {
+    console.log('The access token expires in ' + data.body['expires_in']);
+    console.log('The access token is ' + data.body['access_token']);
+
+    // Save the access token so that it's used in future calls
+    spotifyApi.setAccessToken(data.body['access_token']);
+  },
+  function (err) {
+    console.log('Something went wrong when retrieving an access token', err);
+  }
+);
 
 // var spotifyApi = new SpotifyWebApi();
 
@@ -49,73 +53,136 @@ spotifyApi.clientCredentialsGrant().then(
  */
 
 // Get genre from track
-function getTrackGenre(id, callback) {
-    //console.log("Track ID: " + id)
-  spotifyApi
-    .getTrack(id)
-    .then(function (data) {
-        //console.log("Artist ID: " + data.body.artists[0].id)
-        return spotifyApi.getArtist(data.body.artists[0].id);
-    })
-    .then(function (data) {
-      //console.log(data.body.genres.pop());
-      callback(data.body.genres.pop());
-    })
-    
-    .catch(function (err) {
-      console.error(err);
-    });
-    
-}
+async function getTaste(id) {
+  try {
+    const playlist = await getPlaylistWithTracks(id);
+    let tracksIds = playlist.tracks.items.map((item) => item.track.id);
 
-
-
-function playlistFinder(filter, callback){
-
-    // spotifyApi.searchPlaylists('rap', {
-    //     country: 'US',
-    //     limit : 2,
-    //     offset : 0
-    //   })
-    // .then(function(data) {
-    //   console.log(data.body);
-    // }, function(err) {
-    //   console.log("Something went wrong!", err);
-    // });
-
-    mainData = [];
-
-    spotifyApi.searchPlaylists(filter, {
-        country: 'US',
-        limit : 6,
-        offset : 0
-      })
-    .then(function(data) {
-        //console.log(data)
-        data.body.playlists.items.map(function(item){
-            mainData.push(item);
-            mainData.push('\n')
-            
-            spotifyApi.getPlaylistTracks(item.id).then(function(data){
-                //console.log(data.body.items);
-            })
-            
-        })
-        callback(mainData)
+    console.log(tracksIds);
+    console.log(tracksIds.length);
+    const tracksInfo = [];
+    for (let i = 1; i < tracksIds.length; i++) {
+      if (tracksIds.length >= 50 && i % 50 == 0) {
+        let info = await spotifyApi.getTracks(tracksIds.slice(i - 50, i));
+        //tracksInfo.push(info.body)
+        info.body.tracks.forEach((track) => tracksInfo.push(track.artists[0].id));
+        //console.log(info.body)
+      } else if (tracksIds.length >= 50 && i >= 50 && tracksIds.length - i < 50) {
+        let info = await spotifyApi.getTracks(tracksIds.slice(i - 1, tracksIds.length));
+        info.body.tracks.forEach((track) => tracksInfo.push(track.artists[0].id));
+        break;
+      }
     }
-    // .then(function(playlistID){
-    //     console.log(playlistID)
-    //     return spotifyApi.getPlaylistTracks(playlistID);
-    // })
-    // .then(function(data){
-    //     data.body.items.map(function(values){
-    //         console.log(getTrackGenre(values.track.id))
-    //     })
-    // })
-    , function(err) {
-      console.log("Something went wrong!", err);
-    });
+
+    console.log(tracksInfo);
+
+    let genres = [];
+
+    for (let i = 1; i < tracksInfo.length; i++) {
+      if (tracksInfo.length >= 50 && i % 50 == 0) {
+        let info = await spotifyApi.getArtists(tracksInfo.slice(i - 50, i));
+        //tracksInfo.push(info.body)
+        info.body.artists.forEach((track) => genres.push(track.genres.pop()));
+        //console.log(info.body.artists)
+      } else if (tracksIds.length >= 50 && i >= 50 && tracksIds.length - i < 50) {
+        let info = await spotifyApi.getArtists(tracksInfo.slice(i - 1, tracksInfo.length));
+        info.body.artists.forEach((track) => genres.push(track.genres.pop()));
+        break;
+      }
+    }
+
+    console.log(genres.length);
+
+    let counts = {};
+
+    for (var i = 0; i < genres.length; i++) {
+      var num = genres[i];
+      counts[num] = counts[num] ? counts[num] + 1 : 1;
+    }
+
+    console.log(counts);
+
+    var sum = 0;
+    for (var el in counts) {
+      if (counts.hasOwnProperty(el)) {
+        sum += parseFloat(counts[el]);
+      }
+    }
+
+    console.log('sum: ' + sum);
+
+    const sortable = Object.entries(counts)
+      .sort(([, a], [, b]) => b - a)
+      .reduce((r, [k, v]) => ({ ...r, [k]: v }), {});
+    if (sortable.undefined) {
+      delete sortable.undefined;
+    }
+
+    for (const [key, value] of Object.entries(sortable)) {
+      let words = key.split(' ');
+      let upper;
+      upper = words
+        .map((word) => {
+            if(word == 'uk'){
+                return word.toUpperCase();
+            }
+          return word[0].toUpperCase() + word.substring(1);
+        })
+        .join(' ');
+
+      delete sortable[key];
+      sortable[upper] = value;
+    }
+    sortable['sum'] = sum; 
+    return sortable;
+  } catch (err) {
+    console.log('Something went wrong!', err);
+  }
 }
+
+async function getPlaylistWithTracks(id) {
+  const playlist = (await spotifyApi.getPlaylist(id)).body;
+  // if there is more tracks than the limit (100 by default)
+  if (playlist.tracks.total > playlist.tracks.limit) {
+    // Divide the total number of track by the limit to get the number of API calls
+    for (let i = 1; i < Math.ceil(playlist.tracks.total / playlist.tracks.limit); i++) {
+      const trackToAdd = (
+        await spotifyApi.getPlaylistTracks(id, {
+          offset: playlist.tracks.limit * i, // Offset each call by the limit * the call's index
+        })
+      ).body;
+      // Push the retreived tracks into the array
+      trackToAdd.items.forEach((item) => playlist.tracks.items.push(item));
+    }
+  }
+  return playlist;
+}
+
+async function playlistFinder(filter, offset) {
+  mainData = [];
+  artists = [];
+
+  let playlistData = await spotifyApi.searchPlaylists(filter, {
+    country: 'US',
+    limit: 1,
+    offset: 1,
+  });
+
+  console.log('playlistData', playlistData);
+  playlistData.body.playlists.items.map(async function (item) {
+    // mainData.push(item);
+    // mainData.push('\n');
+    console.log(item.id);
+    const playlist = await getTaste(item.id);
+    console.log('FINAL PLAYLIST' + JSON.stringify(playlist));
+  }),
+    function (err) {
+      console.log('Something went wrong!', err);
+    };
+  // });
+}
+
+async function playlistArray(filter, offset) {}
 
 // scopes = ['user-read-private', 'user-read-email'];
 
@@ -124,79 +191,68 @@ app.get('/', (req, res) => {
   res.send('Hello!');
 });
 
-
 // route for HTTP GET to see the genre of random song of choice
-app.get('/genre-getter', async (req, res) => {
-    //var result = await spotifyApi.getUserPlaylists();
-  getTrackGenre('4DuUwzP4ALMqpquHU0ltAB', function(result){
-    console.log(result)
-    res.send(result);
-  });
-  
+// app.get('/genre-getter', async (req, res) => {
+//   //var result = await spotifyApi.getUserPlaylists();
+//   getTrackGenre('4DuUwzP4ALMqpquHU0ltAB', function (result) {
+//     console.log(result);
+//     res.send(result);
+//   });
+// });
+
+app.get('/my-taste', async (req, res) => {
+  const myTaste = await getTaste('37i9dQZF1DXaXB8fQg7xif');
+  res.send(myTaste);
 });
 
 app.get('/party', async (req, res) => {
-    //var result = await spotifyApi.getUserPlaylists();
-  playlistFinder('party', function(result){
-    console.log(result);
-    res.send(result);
-  })
-  
-  
+  const myTaste = await getTaste('37i9dQZF1DXaXB8fQg7xif');
+
+  //const playlist = await playlistFinder('party');
+  res.send(myTaste);
 });
 
 app.get('/in-my-feels', async (req, res) => {
-    //var result = await spotifyApi.getUserPlaylists();
-  playlistFinder('feels', function(result){
+  //var result = await spotifyApi.getUserPlaylists();
+  playlistFinder('feels', function (result) {
     console.log(result);
     res.send(result);
-  })
-  
-  
+  });
 });
 
 app.get('/on-my-grind', async (req, res) => {
-    //var result = await spotifyApi.getUserPlaylists();
-  playlistFinder('workout', function(result){
+  //var result = await spotifyApi.getUserPlaylists();
+  playlistFinder('workout', function (result) {
     console.log(result);
     res.send(result);
-  })
-  
-  
+  });
 });
 
 app.get('/plotting-my-revenge', async (req, res) => {
-    //var result = await spotifyApi.getUserPlaylists();
-  playlistFinder('motivated', function(result){
+  //var result = await spotifyApi.getUserPlaylists();
+  playlistFinder('motivated', function (result) {
     console.log(result);
     res.send(result);
-  })
-  
-  
+  });
 });
 
 app.get('/romantic', async (req, res) => {
-    //var result = await spotifyApi.getUserPlaylists();
-  playlistFinder('romantic', function(result){
+  //var result = await spotifyApi.getUserPlaylists();
+  playlistFinder('romantic', function (result) {
     console.log(result);
     res.send(result);
-  })
-  
-  
+  });
 });
 
 app.get('/mood-boosters', async (req, res) => {
-    //var result = await spotifyApi.getUserPlaylists();
-  playlistFinder('happy', function(result){
+  //var result = await spotifyApi.getUserPlaylists();
+  playlistFinder('happy', function (result) {
     console.log(result);
     res.send(result);
-  })
-  
-  
+  });
 });
 
-
-app.listen(3000);
+app.listen(3001);
 
 // export the express app we created to make it available to other modules
 // module.exports = app
--- front-end/src/myTaste.js
+++ front-end/src/myTaste.js
@@ -1,62 +1,78 @@
 import './myTaste.css';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Pie } from 'react-chartjs-2';
 import { withRouter } from 'react-router-dom';
+import axios from 'axios';
 
-const state = {
-  labels: ['Hip Hop/Rap', 'Rock', 'Pop', 'R&B', 'Country'],
-  datasets: [
-    {
-      label: 'Rainfall',
-      backgroundColor: ['#B21F00', '#C9DE00', '#2FDE00', '#00A6B4', '#6800B4'],
-      hoverBackgroundColor: ['#501800', '#4B5000', '#175000', '#003350', '#35014F'],
-      data: [43, 23, 20, 13, 1],
-    },
-  ],
-};
+
 
 function MyTaste() {
-  return (
-    <div className='taste'>
-      <h1>My Taste</h1>
+  const [taste, setTaste] = useState('');
+  const [isLoading, setLoading] = useState(true);
+  const url = 'http://localhost:3001/my-taste';
+  useEffect(() => {
+    axios.get(url).then(response => {
+      setTaste(response.data);
+      //setLoading(false);
+    });
+  }, []);
 
-      <div>
-        <Pie
-          data={state}
-          options={{
-            title: {
-              display: true,
-              text: 'Genre Breakdown',
-              fontSize: 20,
-              fontColor: 'white',
-            },
-            legend: {
-              display: true,
-              position: 'right',
-              labels:{
-                  fontColor:'white',
-              }
-            },
-          }}
-        />
+  let other = taste.sum - (Object.values(taste)[0] + Object.values(taste)[1] + Object.values(taste)[2] + Object.values(taste)[3] + Object.values(taste)[4])
+  const state = {
+    labels: [Object.keys(taste)[0],Object.keys(taste)[1],Object.keys(taste)[2],Object.keys(taste)[3],Object.keys(taste)[4], "Other"],
+    datasets: [
+      {
+        label: 'Rainfall',
+        backgroundColor: ['#B21F00', '#C9DE00', '#2FDE00', '#00A6B4', '#6800B4', '#D8D8D8'],
+        hoverBackgroundColor: ['#501800', '#4B5000', '#175000', '#003350', '#35014F', '#888888'],
+        data: [(Object.values(taste)[0]/taste.sum) * 100, (Object.values(taste)[1]/taste.sum) * 100
+        , (Object.values(taste)[2]/taste.sum) * 100, (Object.values(taste)[3]/taste.sum) * 100
+        , (Object.values(taste)[4]/taste.sum) * 100, other],
+      },
+    ],
+  };
+
+    return (
+      <div className='taste'>
+        <h1>My Taste</h1>
+
+        {console.log(taste)}
+
+        <div>
+          <Pie
+            data={state}
+            options={{
+              title: {
+                display: true,
+                text: 'Genre Breakdown',
+                fontSize: 20,
+                fontColor: 'white',
+              },
+              legend: {
+                display: true,
+                position: 'right',
+                labels: {
+                  fontColor: 'white',
+                },
+              },
+            }}
+          />
+        </div>
+        <br />
+
+        <p>Your Library Breakdown by Top 5 Genres:</p>
+
+        <p>{Object.keys(taste)[0]} : {(Object.values(taste)[0]/taste.sum) * 100}%</p>
+
+        <p>{Object.keys(taste)[1]} : {(Object.values(taste)[1]/taste.sum) * 100}%</p>
+
+        <p>{Object.keys(taste)[2]} : {(Object.values(taste)[2]/taste.sum) * 100}%</p>
+
+        <p>{Object.keys(taste)[3]} : {(Object.values(taste)[3]/taste.sum) * 100}%</p>
+
+        <p>{Object.keys(taste)[4]} : {(Object.values(taste)[4]/taste.sum) * 100}%</p>
       </div>
-      <br/>
-
-      <p>Your Library Breakdown by Genre:</p>
-
-      <p>Hip Hop/Rap: 43%</p>
-
-      <p>Rock: 23%</p>
-
-      <p>Pop: 20%</p>
-
-      <p>R&B: 13%</p>
-
-      <p>Country: 1%</p>
-    </div>
-
-    
-  );
+    );
 }
 
 export default MyTaste;
